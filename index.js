8b2aa3a94691c1ebba5a02bf5977330595aefee9
Bilal ÇAĞIRAN <bilal.c@scentrics.uk.com>
2017-05-30 03:04:13 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import PlayerReducer from './src/reducers/player';
+import Scoreboard from './src/containers/Scoreboard';
+
+const store = createStore(
+  PlayerReducer,
+  // Bind to react dev tools (Chrome or Firefox)
+  window.devToolsExtension && window.devToolsExtension()
+);
+
+render(
+  <Provider store={store}>
+    <Scoreboard />
+  </Provider>,
+  document.getElementById('root')
+);
